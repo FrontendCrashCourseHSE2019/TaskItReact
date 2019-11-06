d416130f0e2b4456a85f15daa8b0892c668f9179
h-xanim <44335063+h-xanim@users.noreply.github.com>
2019-11-06 17:08:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import {InputComponent} from "./InputComponent";
 class ToDoItem {
 
     title: string;
+    id : number;
+    date: Date;
 
-    constructor(title: string) {
+    constructor(title: string, index : number) {
         this.title = title;
+        this.id = index;
+        this.date = new Date();
     }
-
 }
 
 interface AppState {
@@ -29,16 +32,38 @@ export class App extends React.Component<{}, AppState> {
     }
 
     onNewTodoHandle(title: string) {
-        let newTodoItem = new ToDoItem(title);
+        let newTodoItem = new ToDoItem(title, this.state.items.length);
         this.setState({
             items: [...this.state.items, newTodoItem]
         });
     }
 
+    onItemDelete(i : any){
+        const items_list: ToDoItem [] = this.state.items.slice();
+        items_list.splice(i, 1);
+        for (var j = i; i < items_list.length; i++) {
+            items_list[i].id = items_list[i].id - 1;
+        }
+        this.setState({
+            items: items_list
+        });
+    }
+
+
     render(): ReactNode {
         return (
             <div className="App">
+                <header className="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
 
+                    <div className="navbar-nav-scroll">
+                        <ul className="navbar-nav bd-navbar-nav flex-row">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Добавить заметки</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </header>
                 <main className="py-md-3 pl-md-5">
 
                     <div className="container">
@@ -48,11 +73,22 @@ export class App extends React.Component<{}, AppState> {
                         <div id="items-container">
                             {this.state.items.map(todoItem => {
                                 return (
-                                    <div>{todoItem.title}</div>
+                                    <div>
+                                        <div className="date-cell">
+                                            <div>{todoItem.title}</div>
+                                            <div className="nav-item">
+                                                <div>{todoItem.date.toLocaleString()}</div>
+                                                <button className="btn btn-danger delete-button w-50" type="button"
+                                                        onClick={() => {
+                                                            this.onItemDelete(todoItem.id)
+                                                        }}>Удалить
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 )
                             })}
                         </div>
-
                     </div>
                 </main>
 
