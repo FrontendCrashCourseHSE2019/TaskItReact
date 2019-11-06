157c6d9d01848024a5ac5ca87ccc7fb265b33ee8
Galina Shalygina <galinushash@gmail.com>
2019-11-06 03:07:24 +0300

    Added date and delete button

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import {InputComponent} from "./InputComponent";
 class ToDoItem {
 
     title: string;
+    date: Date;
+    idx : number;
 
-    constructor(title: string) {
+    constructor(title: string, index : number) {
         this.title = title;
+        this.date = new Date();
+        this.idx = index;
     }
-
 }
 
 interface AppState {
@@ -29,11 +32,23 @@ export class App extends React.Component<{}, AppState> {
     }
 
     onNewTodoHandle(title: string) {
-        let newTodoItem = new ToDoItem(title);
+        let newTodoItem = new ToDoItem(title, this.state.items.length);
         this.setState({
             items: [...this.state.items, newTodoItem]
         });
     }
+
+    onDelete(i : any){
+        const items_list: ToDoItem [] = this.state.items.slice();
+        items_list.splice(i, 1);
+        for (var j = i; i < items_list.length; i++) {
+            items_list[i].idx = items_list[i].idx - 1;
+        }
+        this.setState({
+            items: items_list
+        });
+    }
+
 
     render(): ReactNode {
         return (
@@ -48,11 +63,23 @@ export class App extends React.Component<{}, AppState> {
                         <div id="items-container">
                             {this.state.items.map(todoItem => {
                                 return (
-                                    <div>{todoItem.title}</div>
+                                    <div>
+                                    <div className="d-flex w-100">
+                                        <div>{todoItem.title}</div>
+                                        <div className="d-flex w-100 justify-content-end">
+                                            <div>{todoItem.date.toLocaleString()}</div>
+                                           <button className="btn btn-danger delete-button" type="button"
+                                                    onClick={() => {
+                                                        this.onDelete(todoItem.idx)
+                                                    }}>Delete
+                                           </button>
+                                        </div>
+                                    </div>
+                                        <div>{<span>&nbsp;</span>}</div>
+                                    </div>
                                 )
                             })}
                         </div>
-
                     </div>
                 </main>
 
